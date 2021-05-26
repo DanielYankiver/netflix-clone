5985633356f5d4b2508428c559d5c00046c814d1
Daniel <dyankiver@gmail.com>
2021-05-26 13:47:32 -0400

    almost done with Jumbotron Component

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Container, Inner } from './styles/jumbotron'
+import { Container, Inner, Item, Pane, Title, SubTitle, Image } from './styles/jumbotron'
 
 export default function Jumbotron({ children, direction = 'row', ...restProps}) {
     return (
-        <Inner direction={direction}>
-            {children}
-        </Inner>
+        <Item direction={direction}>
+            <Inner>
+                {children}
+            </Inner>
+        </Item>
     )
 }
 
@@ -13,22 +15,18 @@ Jumbotron.Container = function JumbotronContainer ({ children, ...restProps }) {
     return <Container {...restProps}>{children}</Container>;
 }
 
-Jumbotron.Pane = function JumbotronContainer ({ children, ...restProps }) {
+Jumbotron.Pane = function JumbotronPane ({ children, ...restProps }) {
     return <Pane {...restProps}>{children}</Pane>;
 }
 
-Jumbotron.Title = function JumbotronContainer ({ children, ...restProps }) {
+Jumbotron.Title = function JumbotronTitle ({ children, ...restProps }) {
     return <Title {...restProps}>{children}</Title>;
 }
 
-Jumbotron.SubTitle = function JumbotronContainer ({ children, ...restProps }) {
+Jumbotron.SubTitle = function JumbotronSubTitle ({ children, ...restProps }) {
     return <SubTitle {...restProps}>{children}</SubTitle>;
 }
 
 Jumbotron.Image = function JumbotronContainer ({ children, ...restProps }) {
-    return <Image {...restProps}>{children}</Image>;
-}
-
-Jumbotron.Image = function JumbotronContainer ({ children, ...restProps }) {
-    return <Image {...restProps}></Image>;
+    return <Image {...restProps} />;
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './app';
+import GlobalStyles from 
 
 render(<App />, document.getElementById('root'));
