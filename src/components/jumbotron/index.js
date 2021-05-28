e4582c228758182701f572aac697269e72112cbf
Daniel <dyankiver@gmail.com>
2021-05-28 14:16:18 -0400

    fixed flex direction on item componenet

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -11,10 +11,8 @@ import {
 
 export default function Jumbotron({ children, direction = 'row', ...restProps}) {
     return (
-        <Item direction={direction} {...restProps}>
-            <Inner>
-                {children}
-            </Inner>
+        <Item {...restProps}>
+            <Inner direction={direction}> {children} </Inner>
         </Item>
     )
 }
