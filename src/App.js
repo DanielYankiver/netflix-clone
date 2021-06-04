bc541178b786393fdc16f9fecf3a652fff98e3d7
Daniel <dyankiver@gmail.com>
2021-06-03 21:52:56 -0400

    fixed bug with camelcase in SignIn and Sign Up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Home, Browse, Signin, Signup } from './pages'
+import { Home, Browse, SignIn, SignUp } from './pages';
 import * as ROUTES from './constants/routes';
 
 
 export default function App() {
   return (
     <Router>
-      <Route path="/user">
-          <p>Hello I am a user</p>
+      <Route path="/browse">
+          <Browse />
+      </Route>
+      <Route path="/signin">
+          <SignIn />
+      </Route>
+      <Route path="/signup">
+          <SignUp />
       </Route>
       <Route exact path={ROUTES.HOME} >
         <Home /> 
