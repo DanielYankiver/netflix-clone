64049f8d49178d8d3762a89b905e6823dde1d938
Daniel <dyankiver@gmail.com>
2021-06-07 18:55:06 -0400

    Finished most of Sign in form minus firebase

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -5,8 +5,8 @@ import { Form } from '../components';
 
 
 export default function SignIn() {
-    const [emailAddress, setEmailAddress] = useState();
-    const [password, setPassword] = useState();
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
     const [error, setError] = useState('');
     
     const isInvalid = password === '' || emailAddress === '';
@@ -41,6 +41,13 @@ export default function SignIn() {
                             Sign In 
                         </Form.Submit>
                     </Form.Base>
+
+                    <Form.Text>
+                        New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+                    </Form.Text>
+                    <Form.TextSmall>
+                        This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+                    </Form.TextSmall>
                 </Form>
             </HeaderContainer>
             <FooterContainer></FooterContainer>
