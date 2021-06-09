b567836ee165c7e79ea850b4b0ec04a08a1d9e68
Daniel <dyankiver@gmail.com>
2021-06-09 16:35:02 -0400

    Helpers: routes (react router auth)

--- src/app.js
+++ src/app.js
@@ -2,21 +2,31 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home, Browse, SignIn, SignUp } from './pages';
 import * as ROUTES from './constants/routes';
+import { IsUserRedirect } from './helpers/routes';
 
 export default function App() {
-
-  require('dotenv').config() 
+  const user = {};
 
   return (
     <Router>
-      <Route path="/browse">
+      <IsUserRedirect 
+        user={user} 
+        loggedInPath={ROUTES.BROWSE} 
+        path={ROUTES.SIGN_IN}
+        exact
+      >
+        <SignIn />
+      </IsUserRedirect>
+      <IsUserRedirect 
+        user={user} 
+        loggedInPath={ROUTES.BROWSE} 
+        path={ROUTES.SIGN_UP}
+        exact
+      >
+        <SignUp />
+      </IsUserRedirect>
+      <Route exact path="/browse">
           <Browse />
-      </Route>
-      <Route path="/signin">
-          <SignIn />
-      </Route>
-      <Route path="/signup">
-          <SignUp />
       </Route>
       <Route exact path={ROUTES.HOME} >
         <Home /> 
