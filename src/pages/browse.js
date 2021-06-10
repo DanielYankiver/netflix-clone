53b6e1f5fc2f15b534ff3d8d5a7317f98190d4b3
Daniel <dyankiver@gmail.com>
2021-06-10 10:06:14 -0400

    Utils and directory setup created selectionFilter function

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useContent } from '../hooks';
+import selectionFilter from '../utils/selection-filter';
 
 export default function Browse() {
-    // need series and films 
     const { series } = useContent('series');
     const { films } = useContent('films');
-    // need slides
-    // pass it to the browse container 
+    const slides = selectionFilter({ series, films });
+    console.log(slides)
 
     return (<p>Hello from the browse!</p>);
 }
