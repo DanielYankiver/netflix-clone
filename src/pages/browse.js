3f9be4b1a07d3aa482f8c4d81dfb20beb7c07b6f
Daniel <dyankiver@gmail.com>
2021-06-10 09:34:03 -0400

    Created custom hook useContent for the browse page

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { useContent } from '../hooks';
 
 export default function Browse() {
     // need series and films 
+    const { series } = useContent('series');
+    const { films } = useContent('films');
     // need slides
     // pass it to the browse container 
 
