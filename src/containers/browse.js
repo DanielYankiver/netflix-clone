c1c97f28a44d6e9f8fec0fbd4b499a34472d1368
Daniel <dyankiver@gmail.com>
2021-06-15 14:09:44 -0400

    completed loading in BrowseContainer

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from 'react';
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext } from '../context/firebase';
 import { Loading } from '../components';
+import { Header } from '../components/accordion/styles/accordion';
 
 export function BrowseContainer({ slides }) {
     const [profile, setProfile] = useState({});
@@ -16,7 +17,14 @@ export function BrowseContainer({ slides }) {
     }, [profile.displayName])
 
     return profile.displayName ? (
-        loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />
+
+        <>
+        {loading ? (
+            <Loading src={user.photoURL} /> 
+        ) : ( <Loading.ReleaseBody />
+        )}
+        
+        </>
     ) : (
         <SelectProfileContainer user={user} setProfile={setProfile} />
     );
